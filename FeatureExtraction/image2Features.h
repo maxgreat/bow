579db6123af31097deae83865cc0368dccb94fa6
Maxime Portaz <maxime.portaz@gmail.com>
2016-01-13 18:31:34 +0100

    add c++ class

--- FeatureExtraction/image2Features.h
+++ FeatureExtraction/image2Features.h
@@ -1,4 +1,26 @@
 #ifndef IMAGE2FEATURES_H
 #define IMAGE2FEATURES_H
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+using namespace std;
+using namespace cv;
+
+int image2Features(cv::Mat im, vector<KeyPoint> kp, cv::Mat & descriptors){
+    //Detection of Keypoints
+    FeatureDetector fd = FeatureDetector::create("SIFT");
+    fd.detect(im,kp);
+
+    //Extraction of features
+    DescriptorExtractor de = DescriptorExtractor::create("SIFT");
+    de.compute(im,kp,descriptors);
+
+    return kp.size();
+}
+
+
+
+
 #endif // IMAGE2FEATURES_H
